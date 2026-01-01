7679db8046c27dd06c5ad6df6244eda0a4b5a4e1
Garik Asatryan <asa7ur@proton.me>
2026-01-01 10:51:12 +0100

    Separated admin and manager privileges

--- src/main/java/org/iesalixar/daw2/GarikAsatryan/valkyrfest_admin/controllers/admin/ArtistAdminController.java
+++ src/main/java/org/iesalixar/daw2/GarikAsatryan/valkyrfest_admin/controllers/admin/ArtistAdminController.java
@@ -10,6 +10,7 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,6 +20,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 @RequestMapping("/admin/festival/artists")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('MANAGER')")
 public class ArtistAdminController {
     private final ArtistService artistService;
     private final MessageSource messageSource;
@@ -95,34 +97,3 @@ public class ArtistAdminController {
         return "redirect:/admin/festival/artists";
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
